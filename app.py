b2662c68668ba092e04bb7b8f71424a99fc68663
q1r8 <74010147+q1r8@users.noreply.github.com>
2021-11-21 02:58:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def get_model_response():
         model_response = {'model_response_category_code':predict_category,\
                           'model_response_category_name':category_name,
                           'success': 'ok'}
-    return jsonify(model_response)
+    return jsonify(model_response).headers.add("Access-Control-Allow-Origin", "*")
 
 
 @app.route("/")
