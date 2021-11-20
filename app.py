107630b720426504d064e72c572a46c3a66b1cfd
q1r8 <74010147+q1r8@users.noreply.github.com>
2021-11-21 03:48:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_predicts(model, dataloader):
 
 
 @app.route('/model_inference', methods=['POST', 'GET'])
-@cross_origin(origin='*')
+@cross_origin(support_credentials=True, origin='*')
 def get_model_response():
     print('request_1')
     if request.method == 'POST':
