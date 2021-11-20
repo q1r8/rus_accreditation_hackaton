7623766a38b85180985753728215f0f42b5f0762
egormorozov <egor_morozov2002@mail.ru>
2021-11-21 00:36:43 +0500

    change model loading

--- load_models.sh
+++ load_models.sh
@@ -2,5 +2,6 @@ python3 -m venv venv/
 source venv/bin/activate
 pip install gdown
 
-gdown --id 1R3eM9DNvnhUZtU4FkSA_cMajN7ap6Y4g -O model_files/
+gdown --id 1R3eM9DNvnhUZtU4FkSA_cMajN7ap6Y4g -O ./
+gdown --id 1_zoKVFOoYNy3Axqbs830mgqtrq4smEre -O ./
 unzip models.zip
